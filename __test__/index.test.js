4971f3f26b0c269c770e2d484eb181512e95cb5e
Ivan Lisin <32236977+iFoxMan@users.noreply.github.com>
2020-12-13 14:17:46 +0700

    Initial commit

---
+++ __test__/index.test.js
@@ -0,0 +1,10 @@
+import path from 'path';
+import fs from 'fs';
+import message from '..';
+
+const getFixturePath = (filename) => path.join('__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('', () => {
+  expect(message).toEqual(readFile('expected.txt'));
+});
